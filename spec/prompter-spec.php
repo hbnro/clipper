17e1d077080e8f14f145443b74620c0fea7f5bea
Alvaro Cabrera <pateketrueke@gmail.com>
2016-02-01 15:18:19 -0600

    Fixed all specs

--- spec/prompter-spec.php
+++ spec/prompter-spec.php
@@ -2,102 +2,103 @@
 
 describe('Prompt helpers:', function () {
     beforeEach(function () {
-        $stdout = '';
-
-        $context = spy('\\Clipper\\Shell')
+        $shell = spy('Clipper', 'Shell')
             ->methods('clear', 'write', 'readln', 'flush')
             ->callOriginalMethods(false)
             ->getMock();
 
-        $context->method('write')
-            ->will(returnCallback(function () use (&$stdout) {
-                $stdout .= implode('', func_get_args());
-            }));
+        $shell->method('clear')->will(returnSelf());
+        $shell->method('flush')->will(returnSelf());
 
-        $prompter = new \Clipper\Prompter($context);
+        $shell->expects($write = any())
+            ->method('write')
+            ->will(returnSelf());
 
-        let('stdout', function () use (&$stdout) {
-            return $stdout;
+        let('shell', $shell);
+
+        let('readln', function ($value = null, $count = 1) use ($shell) {
+            $shell->expects($readln = exactly($count))
+                ->method('readln')
+                ->will(returnValue($value));
+
+            return $readln;
         });
 
-        let('prompter', $prompter);
+        let('stdout', function () use ($write) {
+            return implode('', array_map(function ($call) {
+                return implode('', $call->parameters);
+            }, $write->getInvocations()));
+        });
+
+        let('prompt', new \Clipper\Prompter($shell));
     });
 
-    it('should wait for any key', function ($stdout, $prompter) {
-        $prompter->wait();
+    it('should wait for any key', function ($stdout, $prompt) {
+        $prompt->wait();
         expect($stdout())->toEqual("Press any key\n");
     });
 
-    it('should wait until N seconds', function ($stdout, $prompter) {
-        $prompter->wait(1);
-        expect($stdout())->toEqual("1s...0s...\n");
+    it('should wait until N seconds', function ($stdout, $prompt) {
+        fn('Clipper', 'sleep')
+            ->expects(exactly(3));
+
+        $prompt->wait(3);
+
+        expect($stdout())->toEqual("3s...2s...1s...0s...\n");
     });
 
-    xit('should prompt for any value', function ($context, $prompter) {
-        $readln = $context->__spy('readln', 'bar');
-        $input = $prompter->prompt('foo');
-        $args = $readln();
+    it('should prompt for any wanted value', function ($readln, $prompt) {
+        $stub = $readln('bar');
+        $input = $prompt->prompt('foo');
+        $calls = $stub->getInvocations();
 
         expect($input)->toEqual('bar');
-        expect(implode('', $args))->toEqual('foo: ');
+        expect(implode('', end($calls)->parameters))->toEqual('foo: ');
     });
 
-    xit('should prompt for some value with defaults', function ($context, $prompter) {
-        $readln = $context->__spy('readln');
-        $input = $prompter->prompt('foo', 'bar');
-        $args = $readln();
+    it('should prompt for some value with defaults', function ($readln, $prompt) {
+        $stub = $readln();
+        $input = $prompt->prompt('foo', 'bar');
+        $calls = $stub->getInvocations();
 
         expect($input)->toEqual('bar');
-        expect(implode('', $args))->toEqual('foo [bar]: ');
+        expect(implode('', end($calls)->parameters))->toEqual('foo [bar]: ');
     });
 
-    xit('should allow to choice simple options', function ($context, $prompter) {
-        $readln = $context->__spy('readln');
-        $input = $prompter->choice('foo');
-        $args = $readln();
+    it('should allow to choice simple options', function ($readln, $prompt) {
+        $stub = $readln();
+        $input = $prompt->choice('foo');
+        $calls = $stub->getInvocations();
 
         expect($input)->toEqual('n');
-        expect(implode('', $args))->toEqual('foo [y/N]: ');
+        expect(implode('', end($calls)->parameters))->toEqual('foo [y/N]: ');
     });
 
-    xit('should allow to choice custom options with defaults', function ($context, $prompter) {
-        $readln = $context->__spy('readln', 'a');
-        $input = $prompter->choice('foo', 'abc', 'c');
-        $args = $readln();
+    it('should allow to choice custom options with defaults', function ($readln, $prompt) {
+        $stub = $readln('a');
+        $input = $prompt->choice('foo', 'abc', 'c');
+        $calls = $stub->getInvocations();
 
         expect($input)->toEqual('a');
-        expect(implode('', $args))->toEqual('foo [a/b/C]: ');
+        expect(implode('', end($calls)->parameters))->toEqual('foo [a/b/C]: ');
     });
 
-    xit('should allow to pick values from a simple menu', function ($context, $prompter) {
-        $readln = $context->__spy('readln');
-        $input = $prompter->menu(array('a', 'b', 'c'));
-        $args = $readln();
+    it('should allow to pick values from a simple menu', function ($readln, $stdout, $prompt) {
+        $stub = $readln();
+        $input = $prompt->menu(array('a', 'b', 'c'));
+        $calls = $stub->getInvocations();
 
         expect($input)->toEqual(-1);
-        expect($context->__stdout)->toEqual('
-  1. a
-  2. b
-  3. c
-
-');
-        expect(implode('', $args))->toEqual('Choose one: ');
+        expect($stdout())->toEqual("\n  1. a\n  2. b\n  3. c\n\n");
+        expect(implode('', end($calls)->parameters))->toEqual('Choose one: ');
     });
 
-    xit('should wrap long text as necessary', function ($context, $prompter) {
-        $context->width = 40;
-        $prompter->wrap('Lorem ipsum dolor sit amet, consectetur-adipisicing-elit-sed-do-eiusmod');
+    it('should wrap long text as necessary', function ($shell, $stdout, $prompt) {
+        $shell->width = 40;
+        $prompt->wrap('Lorem ipsum dolor sit amet, consectetur-adipisicing-elit-sed-do-eiusmod');
 
-        expect($context->__stdout)->toEqual('
-  Lorem ipsum dolor sit amet,
-  consectetur-adipisicing-elit-sed-do-e
-  iusmod
-');
+        expect($stdout())->toEqual("\n  Lorem ipsum dolor sit amet,\n  consectetur-adipisicing-elit-sed-do-e\n  iusmod\n");
     });
 
-    xit('.', function ($context, $prompter) {
-        $context->width = 10;
-        $prompter->progress(2, 5);
-        #expect($context->__stdout)->toContain('x'); //, "<c:cyan,cyan>|</c><c:light_gray,light_gray>-</c>  41%");
-    });
+    // TODO: progress
 });
